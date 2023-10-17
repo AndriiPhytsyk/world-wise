f513d0dc392a19ddf929205372fa1ba149efdc9a
Andrii <phytsyk@gmail.com>
2023-10-17 13:21:54 +0200

    added basic layout, styles and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="product" element={<Product/>}/>
+                <Route path="pricing" element={<Pricing/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="cities" element={<p></p>}/>
+                <Route path="app" element={<AppLayout/>}>
+                    <Route path="cities" element={<p>List of cities</p>}/>
+                    <Route path="countries" element={<p>Countries</p>}/>
+                    <Route path="cities" element={<p>Form</p>}/>
+                 </Route>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
